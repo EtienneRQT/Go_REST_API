cac76fe73d13598a113a4a540be23eb8c15af42e
EtienneRQT <etienne.riquet@sfr.fr>
2024-02-25 21:16:19 +0100

    creating routes to handle getting and creating events

--- main.go
+++ main.go
@@ -1,16 +1,31 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"example.com/api/models"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	server := gin.Default()
-server.GET("/events", getEvents)	
+server.GET("/events", getEvents)
+	server.POST("/events", createEvent)
 
-	server.Run("localhost:8080")
+server.Run("localhost:8080")
 }
 
 func getEvents(context *gin.Context)  {
-	context.JSON(200, gin.H{
-		"events": []string{"Meeting", "Lunch", "Party"},
-	})
+	events := models.GetAllEvents()
+	context.JSON(http.StatusOK, events)
+}
+
+func createEvent(context *gin.Context)  {
+	var event models.Event
+	if err := context.BindJSON(&event); err != nil {
+		context.JSON(http.StatusBadRequest, err.Error())
+		return
+	}
+	event.Save()
+	context.JSON(http.StatusCreated, event)
 }
